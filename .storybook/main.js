f0cb1ba4498a2352a40932f0384f4c75b8c7fd72
yukishinonome <yuki.yamakoshi0126@gmail.com>
2021-03-14 23:05:10 +0900

    sass読み込むためのローダー設定

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,5 +3,18 @@ module.exports = {
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials']
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: [
+        path.resolve(__dirname, '../styles'),
+        path.resolve(__dirname, '../components'),
+        path.resolve(__dirname, '../node_modules')
+      ]
+    })
+
+    return config
+  }
 }

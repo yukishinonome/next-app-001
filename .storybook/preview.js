6ee2a8285fb9912e2f9f36f2488659c9ea825075
yukishinonome <yuki.yamakoshi0126@gmail.com>
2021-03-14 23:22:01 +0900

    next/imageがstorybookで使えないので置き換える

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,32 @@
+import * as nextImage from 'next/image'
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' }
 }
+
+// next/imageそのままだとStorybookで使えないので置き換える
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: (props) => {
+    const { width, height } = props
+    const ratio = (height / width) * 100
+    return (
+      <div
+        style={{
+          paddingBottom: `${ratio}%`,
+          position: 'relative'
+        }}
+      >
+        <img
+          style={{
+            objectFit: 'cover',
+            position: 'absolute',
+            width: '100%',
+            height: '100%'
+          }}
+          {...props}
+        />
+      </div>
+    )
+  }
+})
